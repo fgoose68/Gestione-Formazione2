4dd29e466dc955d522131c89a7f3bbb6017c9d30
[dyad] <git@dyad.sh>
2025-05-07 19:12:52 +0200

    [dyad] Creata applicazione GESTIOFORMAZIONE con dashboard, calendario e gestione eventi - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import NewEvent from "./pages/NewEvent";
+import CalendarPage from "./pages/CalendarPage";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +18,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/nuovo-evento" element={<NewEvent />} />
+          <Route path="/calendario" element={<CalendarPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
