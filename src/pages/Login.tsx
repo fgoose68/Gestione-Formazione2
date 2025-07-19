8a2848e520d837f09575b73dc049e0b072b1c227
[dyad] <fgoose68@libero.it>
2025-07-19 12:16:12 +0200

    [dyad] Aggiornata la versione nella pagina di login - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -89,7 +89,7 @@ function Login() {
         
         {/* Versione aggiornata */}
         <div className="absolute bottom-2 right-4 text-xs text-gray-500 z-20">
-          Ver.9.2Lug25
+          Ver.19.1Lug25
         </div>
       </div>
     </div>
