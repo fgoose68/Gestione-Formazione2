4706c41ae04ca4dab219d4a410e7fc7dc82db63c
[dyad] <fgoose68@libero.it>
2025-12-26 11:18:12 +0100

    [dyad] Mostrate le regole statiche delle scadenze per i corsi standard. - wrote 1 file(s)

--- src/pages/DeadlinesPage.tsx
+++ src/pages/DeadlinesPage.tsx
@@ -78,7 +78,7 @@ const DeadlinesPage = () => {
 
   // Filtra le scadenze imminenti per tipo di corso
   const standardUpcomingDeadlines = useMemo(() => {
-    const standardTypes: Deadline['type'][] = ['docente', 'discenti_standard', 'avvio_standard', 'giorno_evento_registri', 'post_evento_feedback', 'post_evento_modello_l'];
+    const standardTypes: Deadline['type'][] = ['docente', 'discenti_standard', 'avvio_standard', 'giorno_evento_registri', 'post_evento_feedback', 'post_evento_modello_l', 'risposte_reparti']; // Aggiunto risposte_reparti
     return sortedUpcomingDeadlines.filter(d => standardTypes.includes(d.type));
   }, [sortedUpcomingDeadlines]);
 
